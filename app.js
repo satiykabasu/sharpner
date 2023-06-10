3544a91003ec03fe07727614c4f547f5147a4d4c
satiyka basu <122592020+satiykabasu@users.noreply.github.com>
2023-06-10 09:10:55 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 console.log("Hello , welcome to the sharpner");
 console.log("this is version control history");
+console.dir(document);//display all the properties and method associated with document obj 
+console.log("document.domain");
+console.log(document.title);
+console.log(document.all)//it gives all array and html element associated with document obj
+//it shows how all html element associated with index no,you can change the property via index but it
+//is not good choice as if i change or add any html element then it will change those style
+console.log(document.form)//accessed via index
+console.log(document.image);
+let value=document.getElementById('header-title');
+console.log(value.innerText);//it focus on style for span
+console.log(value.textContent);//does not focus on style
+//value.innerHTML='<h3>Hello</h3>'
+//onsole.log(value.innerHTML)
+value.style.borderBottom='solid 3px black';
+let value2=document.getElementById('first');
+value2.style.fontSize='bold';
+value2.style.color='green';
+
+
